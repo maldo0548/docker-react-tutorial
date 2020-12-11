0edd501064ecbd3a6b27090ddc7fa8a8b7470a06
Miguel Maldonado <maldonado.miguel45@gmail.com>
2020-12-11 15:02:53 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,14 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Nick</code> is a smart fool.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://random.dog" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
